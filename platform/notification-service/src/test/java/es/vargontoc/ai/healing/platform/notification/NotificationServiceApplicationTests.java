67057b98db9023194ab4318bba94ed3d84acc2dc
Vargontoc <atperamato@gmail.com>
2026-01-27 20:17:08 +0100

    feat(notifications) New service

---
+++ platform/notification-service/src/test/java/es/vargontoc/ai/healing/platform/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.vargontoc.ai.healing.platform.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
